7ba5c6b4511bbb364201969f98411d7a10227b7f
Jennet <mankahjane2019@gmail.com>
2022-03-27 21:57:17 -0400

    completed assignnment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution here!
+const { expect } = require("chai");
+
+function indexjs() {};
+function cats() {};
+function indexjscats([Milo, Otis, Garfield]) {
+    expect [Milo, Otis, Garfield]
+};
+function array() {}
+function beforeEach() {
+cats.length = 0
+cats.push = (Milo, Otis, Garfield)
+}
+function destructivelyAppendCat(name) {
+
+}
